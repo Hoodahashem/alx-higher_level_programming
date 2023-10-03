3aa3727d3258e999562c8ca99f2d5e3911e5e5fb
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-04 01:53:20 +0300

    Gustavo on the process!

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    var = ord(c)
-    if var in range(65, 91):
-        return True
-    else:
-        return False
+    def uppercaselol(chrs):
+        var = ord(chrs)
+        if var in range(65, 91):
+            return chrs
+        else:
+            return chr(var + 31)
+    for lol in str:
+        print(uppercaselol(lol), end="")
+    print("\n", end="")
