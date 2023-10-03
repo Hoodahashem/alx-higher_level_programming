c1a6871ef44b414d04aeb6d6b2d0adb365c1cdff
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-04 02:39:17 +0300

    Gustavo on the process!

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-# Function print_last_digit(number)
-#        Convert the number to a string
-#     string_number = Convert number to string
 
-#        Get the last character of the string
-#     last_character = Get the last character of string_number
-
-#        Convert the last character back to a number
-#     last_digit = Convert last_character to number
-    #    Print the last digit
-#     Print last_digit
-
-# End Function
 def print_last_digit(number):
     if number < 0:
         var = number % -10
