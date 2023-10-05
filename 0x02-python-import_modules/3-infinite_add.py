5cc43481a931f2629ddb76de54f5e2f4b78f0044
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-06 02:39:58 +0300

    Gus is on the process!

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    r = 0
-    for i in range(1, len(sys.argv)):
-        r += int(sys.argv[i])
-    print("{}".format(r))
+    l = len(sys.argv)
+    s = 0
+    if l == 1:
+        s = 0
+    else:
+        for i in range(1, l):
+            s += int(sys.argv[i])
+    print("{:d}".format(s))
