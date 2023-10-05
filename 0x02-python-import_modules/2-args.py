b9d674e77a5582fc35e11c0fa0054412ebe522ee
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-06 02:32:44 +0300

    Gus is on the process!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+from sys import argv
+if __name__ == "__main__":
+    index = len(sys.argv)
+    if index == 1:
+        print("{:d} arguments.".format(index - 1))
+    elif index == 2:
+        print("{:d} argument:".format(index - 1))
+    else:
+        print("{:d} arguments:".format(index - 1))
+    for i in range(1, index):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
