5d7ee04b788213bf3f25a4d89e487257118272c8
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-06 02:49:15 +0300

    Gus is on the process!

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import sys
+
 if __name__ == "__main__":
-    print(sum(int(arg) for arg in sys.argv[1:]))
+    import sys
+    r = 0
+    for i in range(len(sys.argv) - 1):
+        r += int(sys.argv[i + 1])
+    print("{}".format(r))
