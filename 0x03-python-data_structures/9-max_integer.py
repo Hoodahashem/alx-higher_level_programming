22158b5bf44db611085ececcaf74abfc3d95529c
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-06 18:56:19 +0300

    Gus is on the process!

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        fornow = my_list[0]
+        for i in range(1, len(my_list)):
+            if fornow < my_list[i]:
+                fornow = my_list[i]
+        return fornow
