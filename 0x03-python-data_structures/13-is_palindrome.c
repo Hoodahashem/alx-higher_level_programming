f07795522462f32cc3dc39674d844d935819bc5d
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-09 20:49:43 +0300

    studying for the interview question!

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,45 @@
 #include "lists.h"
+/**
+ * is_palindrome - function
+ * @head: the head of the list
+ * Return: true if the head of the list is a palindrome
+*/
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	listint_t *slow = *head, *fast = *head;
 
-// Plan:
-// Find the middle of the linked list.
-// Reverse the second half of the linked list.
-// Compare the first half and the reversed second half.
-// If they are identical, then the linked list is a palindrome.
-// Reverse the second half again to restore the original linked list.
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	listint_t *prev = NULL, *curr = slow, *next;
 
-int is_palindrome(listint_t **head) {
-    if (*head == NULL || (*head)->next == NULL)
-        return 1;
-    listint_t *slow = *head, *fast = *head;
-    while (fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    listint_t *prev = NULL, *curr = slow, *next;
-    while (curr != NULL)
-    {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    listint_t *fhalf = *head, *shalf = prev;
-    while (shalf != NULL)
-    {
-        if (fhalf->n != shalf->n)
-            return 0;
-        fhalf = fhalf->next;
-        shalf = shalf->next;
-    }
-    curr = prev, prev = NULL;
-    while (curr != NULL)
-    {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return 1;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	listint_t *fhalf = *head, *shalf = prev;
+
+	while (shalf != NULL)
+	{
+		if (fhalf->n != shalf->n)
+			return (0);
+		fhalf = fhalf->next;
+		shalf = shalf->next;
+	}
+	curr = prev, prev = NULL;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	return (1);
 }
