ade3be626e404efcc0dac513cac4c52af4da2af8
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-11 10:03:53 +0300

    Gus is no the process!

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    indexes  = []
+
+    for i in range(len(my_list)):
+        if (my_list[i] == search):
+            indexes.append(i)
+    for x in indexes:
+        my_list[x] = replace
+    return my_list
