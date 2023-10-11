2276f9fd4773d19a2e5a2e549be3b520ab1c66ab
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-11 10:30:47 +0300

    Gus is no the process!

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    
+    sum = 0
+    for i in my_list:
+        x = my_list.count(i)
+        if x > 1:
+            my_list.remove(i)
+    for j in my_list:
+        sum += j
+    return sum
+
