5ed4dfe9000a65ff5e4ae0a0080d33e174c274b2
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-12 19:55:06 +0300

    Gus Is On The Process!

--- 0x17-doubly_linked_lists/learn.c
+++ 0x17-doubly_linked_lists/learn.c
@@ -24,6 +24,7 @@ void dalloc(node **tail, node **head)
     tail = NULL;
     head = NULL;
 }
+
 void add_at_the_beginning(node **tail, int value)
 {
     node *newnode = malloc(sizeof(node));
@@ -37,6 +38,7 @@ void add_at_the_beginning(node **tail, int value)
     }
     *tail = newnode;
 }
+
 void add_at_the_end(node **head, int value)
 {
     node *newnode = malloc(sizeof(node));
@@ -50,6 +52,7 @@ void add_at_the_end(node **head, int value)
     }
     *head = newnode;
 }
+
 void init(node **head, node **tail, int value)
 {
     node *newnode = malloc(sizeof(node));
@@ -60,6 +63,7 @@ void init(node **head, node **tail, int value)
     *tail = newnode;
     *head = newnode;
 }
+
 void add_after(node *btngan, int value)
 {
     node *newnode = malloc(sizeof(node));
@@ -72,6 +76,34 @@ void add_after(node *btngan, int value)
     }
     btngan->next = newnode;
 }
+
+void remove_node(node *btngan)
+{
+    if (btngan->prev != NULL)
+    {
+        btngan->prev->next = btngan->next;
+    }
+    if (btngan->next != NULL)
+    {
+        btngan->next->prev = btngan->prev;
+    }
+    free(btngan);
+}
+
+node *find_node(node *btngan, int value)
+{
+    node *curr = btngan;
+    while(btngan != NULL)
+    {
+        if (curr->x = value)
+        {
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
+
 int main(void)
 {
     node *tail = NULL;
@@ -80,11 +112,14 @@ int main(void)
     init(&head, &tail, 3);
     add_at_the_end(&head, 5);
     add_after(head->prev, 4);
-    node *curr = tail;
-    while (curr != NULL)
-    {
-        printf("the number is: %d\n", curr->x);
-        curr = curr->next;
-    }
+    remove_node(head->prev);
+    add_at_the_end(&head, 100);
+    node *find = find_node(tail, 3);
+    // node *curr = tail;
+    // while (curr != NULL)
+    // {
+    //     printf("the number is: %d\n", curr->x);
+    //     curr = curr->next;
+    // }
     dalloc(&tail, &head);
 }
