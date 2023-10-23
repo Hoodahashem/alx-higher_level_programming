ccb166750ce11b890bc51935223400491e8f9a50
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-23 17:27:04 +0300

    Gus is on the process!

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         div = a / b
         return div
-    except:
+    except (ValueError, ZeroDivisionError):
         div = None
         return None
     finally:
