d9fc8fa2c27b2801a4ba2b465cbe726e14f5fd4a
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-23 16:56:33 +0300

    Gus is on the process!

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    noe = 0
-    ll = 0
-    for item in my_list:
-        ll += 1
-    try:
-        if (x == 0):
-            return noe
-        if (x >= ll):
-            for elem in my_list:
-                print("{}".format(elem), end="")
-            print()
-            return ll
-        else:
-            for elem in my_list:
-                noe += 1
-                print("{}".format(elem), end="")
-                if (noe == x):
-                    break
-            print()
-            return noe
-    except BaseException:
-        pass
+    index = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            index += 1
+        except IndexError:
+            break
+    print("")
+    return (index)
