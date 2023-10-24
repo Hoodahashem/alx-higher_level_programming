7fe77713b3d1e6d0e012dfaa45da7c083032f07b
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-24 20:31:45 +0300

    Gus is on the process!

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""there is no fucking modules!"""
+
+
+class Square:
+    """class gustavo"""
+
+    def __init__(self, size=0):
+        """Initialize a new gustavo
+            size (int): gustavo
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        property
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return gustavo"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints the #"""
+        for i in range(0, self.__size):
+            for x in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
