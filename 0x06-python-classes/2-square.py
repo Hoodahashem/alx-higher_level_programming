0c6255fafe1066fe63a03c3891229890cbc40596
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-24 19:24:42 +0300

    Gus is on the process!

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,13 +9,8 @@ class Square:
         """ fucking square class
             size (int): gustavo
         """
-        try:
-            if type(size) != int:
-                raise TypeError
-            if size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
