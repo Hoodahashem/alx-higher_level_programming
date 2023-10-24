c0c33c40a93712a57344b678a77df237cd2267bc
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-24 19:04:26 +0300

    Gus is on the process!

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" def class Square."""
+
+
+class Square:
+    """ this is class square."""
+    def __init__(self, size):
+        """
+            size : gustavo
+        """
+        self.size = size
