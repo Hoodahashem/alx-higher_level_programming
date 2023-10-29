efda9b491e59c0fb960ce95b94f9b72656efcae1
Hoodahashem <mahmoudhashem335@gmail.com>
2023-10-29 22:21:36 +0200

    Gus Is On The Process!

--- 0x07-python-test_driven_development/add_integer.py
+++ 0x07-python-test_driven_development/add_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""don't give a shit about anything anybody focus on yourself!"""
 def add_integer(a, b=98):
-    if not isinstance(a, int):
+    """
+    Return the integer addition of a and b.
+    Raises: TypeError
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    int(a)
-    int(b)
-    return a + b
+    return (int(a) + int(b))
