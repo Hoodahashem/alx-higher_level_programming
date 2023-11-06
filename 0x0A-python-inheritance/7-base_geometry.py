002980e35e37c77a6f2a0b9e0628ff213275e3fa
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-07 00:49:26 +0200

    done

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     def area(self):
         """docstring for area"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """docstring for integer_validator"""
         if type(value) != int:
