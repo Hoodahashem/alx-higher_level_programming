c778cfd62cd74b102379b5811002e4af8d7d8c28
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-07 00:32:51 +0200

    the 7th task is done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Gus is on the process"""
+
+
+class BaseGeometry:
+    """docstring for BaseGeometry"""
+    def area(self):
+        """docstring for area"""
+        raise Exception("area() is not implemented")
