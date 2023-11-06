17a7a1d9abed134d2b33821936f2fd4b28462846
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-06 22:52:29 +0200

    let's see if it works

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""2th task"""
+
+
+class MyList(list):
+    """docstring for MyList"""
+    def print_sorted(self):
+        """docstring for print_sorted"""
+        print(sorted(self))
