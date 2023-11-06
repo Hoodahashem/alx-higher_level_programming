1e87a37e67625eb3e1c2a262527c65a35a878356
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-07 01:14:38 +0200

    try 3

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,4 +9,5 @@ class Square(Rectangle):
     """docstring for Square"""
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
