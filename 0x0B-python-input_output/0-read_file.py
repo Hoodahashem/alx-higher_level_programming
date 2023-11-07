a22ca1604464baea65f61f44e7de9534f405d2e3
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-07 23:51:26 +0200

    try 5 on the 1th task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,4 @@
 def read_file(filename=""):
     """Read a file"""
     with open(filename, "r", encoding="utf-8") as f:
-        returns = f.read()
-        f.close()
-    return returns
+        return f.read()
