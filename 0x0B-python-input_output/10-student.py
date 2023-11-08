c71b78cf9d84f298995050b6f30395fd3ba6fcf4
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-08 03:02:41 +0200

    try 2 on task number 11

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,8 +9,13 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        if attrs == None:
-            return self.__dict__
-        else:
-            return {attr: self.__dict__[attr] for attr in attrs}
+def to_json(self, attrs=None):
+    """Convert a Python object to a JSON string"""
+    if attrs is None:
+        return self.__dict__
+    else:
+        my_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                my_dict[attr] = getattr(self, attr)
+        return my_dict
