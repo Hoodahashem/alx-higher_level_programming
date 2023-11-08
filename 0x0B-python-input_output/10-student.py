d6a9e5f9669b288a49293dc734cee5081e729463
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-08 03:24:49 +0200

    pypass the task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,7 +11,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """Converts attributes to json representation"""
-        if attrs == None:
+        if attrs is None:
             return self.__dict__
         else:
             dict = {}
