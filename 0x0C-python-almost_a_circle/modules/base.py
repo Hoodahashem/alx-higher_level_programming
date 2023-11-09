f0e18570c9c3851d0ec3d7e8edbee2761d149aaf
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-09 04:34:44 +0200

    try 2 on the 1st task

--- 0x0C-python-almost_a_circle/modules/base.py
+++ 0x0C-python-almost_a_circle/modules/base.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-"""i'm just working on myself and hoping from GOD that gives me what i'm looking for."""
+"""i'm just working on myself and hoping from GOD that gives me
+what i'm looking for."""
 
 
 class Base:
     """stay hard and let the GOD DO HIS JOB AND JRUST HIM"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Initialize a new instance of the class"""
         if id is None:
