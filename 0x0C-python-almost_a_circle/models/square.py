10bac83a96e5d1dfd7350f56929d3e3b87020a0f
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-10 19:52:39 +0200

    let's wrap this project quickly

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# rectangle.py
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if args and len(args)!= 0:
+            x = 0
+            for arg in args:
+                if x == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif x == 1:
+                    self.size = arg
+                elif x == 2:
+                    self.x = arg
+                elif x == 3:
+                    self.y = arg
+                x += 1
+        elif kwargs and len(kwargs) != 0:
+            if 'id' in kwargs:
+                if kwargs[id] == None:
+                    self.__init__(self.size, self.x, self.y)
+                else:
+                    self.id = kwargs[id]
+            if'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
