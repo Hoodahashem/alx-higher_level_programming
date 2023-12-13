a24ed66d37088fbc7d4184c110e48bfdf059be01
Hoodahashem <mahmoudhashem335@gmail.com>
2023-12-13 22:49:00 +0200

    done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- STATES.SQL
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(
-    `id` INT UNIQUE NOT NULL PRIMARY KEY,
+    PRIMARY KEY(`id`),
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
-)
+);
