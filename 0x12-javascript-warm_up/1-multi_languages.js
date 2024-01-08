f828a97400155b25937295460caafd219828b5c2
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-08 23:35:46 +0200

    again with constant

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+const myVar = 'C is fun';
+const myVar2 = 'Python is cool';
+const myvar3 = 'JavaScript is amazing';
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log(myVar);
+console.log(myVar2);
+console.log(myvar3);
