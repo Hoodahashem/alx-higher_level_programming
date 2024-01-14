8e69865da9124b5501f7128b101fba2261748f61
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-14 17:02:21 +0200

    7th task is done

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""import the needed modules!"""
+
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class State(Base):
+    """initiating the State class"""
+
+    __tablename__ = states
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
