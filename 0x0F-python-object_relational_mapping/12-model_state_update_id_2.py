978a2652d9ba552ec47b30f1048eb244997dee10
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-14 18:35:02 +0200

    again

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if state.id == 2:
-            state.name = "New Mexico"
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
