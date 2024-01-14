464ec3fe395dbc94fd99a6be038f9c686fd44c7c
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-14 17:05:22 +0200

    again

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""import the needed modules!"""
 
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+""" 
+importing the needed modules
+"""
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
 class State(Base):
-    """initiating the State class"""
-
-    __tablename__ = states
-
+    """
+    Represents a state for a MySQL database.
+    """
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
