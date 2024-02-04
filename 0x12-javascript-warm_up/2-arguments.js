1d8de8c313e14d096a6dd4538c2dc7b8f57af81a
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-04 04:52:23 +0200

    again

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length === 1){
-  console.log('Argument found');
+} else if (process.argv.length === 1) {
+  console.log('No argument');
 } else {
   console.log('Argument found');
 }
