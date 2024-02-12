e296aa7c63d5b38846711631a1c6c3486605aaac
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-12 22:15:23 +0200

    modified

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,20 @@
 #!/usr/bin/node
 
-if (isNaN(process.argv[2]) || process.argv.length <= 3)
-{
-  console.log(0);
-} else {
-  let biggest = 0;
-  for (let i = 2; i < process.argv.length; i++)
-  {
-    if (biggest < process.argv[i])
-    {
-      biggest = process.argv[i]
+function second (myArray) {
+  if (myArray.length === 2 || myArray.length === 3) { return (0); }
+
+  let max = myArray[2];
+  let secondMax = myArray[3];
+
+  for (let i = 2; i < myArray.length; i++) {
+    if (myArray[i] > max) {
+      secondMax = max;
+      max = myArray[i];
+    } else if (myArray[i] > secondMax && myArray[i] < max) {
+      secondMax = myArray[i];
     }
   }
-  let secondbiggest = 0
-  for (let x = 0; x < process.argv.length; x++) {
-    if (process.argv[x] !== biggest && secondbiggest < process.argv[x])
-    {
-      secondbiggest = process.argv[x]
-    }
-  }
-  console.log(secondbiggest)
+  return (secondMax);
 }
+
+console.log(second(process.argv));
