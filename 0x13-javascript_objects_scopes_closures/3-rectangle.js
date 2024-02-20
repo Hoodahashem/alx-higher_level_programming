84fa88ca318b77b98140abaafdfb06e4af406094
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-20 17:54:46 +0200

    semistandard!

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,17 +3,17 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !h || !w) {
-      return this
+      return this;
     } else {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       for (let x = 0; x < this.width; x++) {
-        process.stdout.write("X");
+        process.stdout.write('X');
       }
       process.stdout.write('\n');
     }
