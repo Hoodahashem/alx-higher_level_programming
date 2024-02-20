b5b8aef207cbcf52f11a02318198aa738684b857
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-20 19:54:27 +0200

    done with 2nd task!

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const file = process.argv[2];
+const fs = require('fs');
+const toWrite = process.argv[3];
+
+fs.writeFile(file, toWrite, 'utf-8', function (error) {
+  if (error) {
+    console.log(error)
+  }
+});
