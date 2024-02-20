f1b8b2cd82d233df9e261a354da1ae7cbdd6e338
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-20 19:11:23 +0200

    task number 9!

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let rList = []
+  const rList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    rList.push(list[i])
+    rList.push(list[i]);
   }
-  return rList
-}
+  return rList;
+};
