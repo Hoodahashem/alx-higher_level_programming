b4eb570c0cd11c877ea782fe16cc8153998ffc6c
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-20 19:28:32 +0200

    fixing the bug!

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
+const file = process.argv[2];
 const fs = require('fs');
 
-fs.readFile('file.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
